cb556d7e1e04fe15d7b2a7858b5702f00dd31009
nciganovic <nciganovic99@gmail.com>
2020-09-27 10:53:55 +0200

    place code for windows game input in sepate file

--- driver.py
+++ driver.py
@@ -1,56 +1,9 @@
 import numpy as np
 import cv2 
 import time
-import ctypes
+import platform
+import winkeys as wk #Keaboard keys for playing games on Windows
 from pynput.keyboard import Key, Controller
-
-SendInput = ctypes.windll.user32.SendInput
-PUL = ctypes.POINTER(ctypes.c_ulong)
-class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),
-                ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong),
-                ("wParamL", ctypes.c_short),
-                ("wParamH", ctypes.c_ushort)]
-
-class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
-
-class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("ii", Input_I)]
-
-def PressKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-def ReleaseKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-hex_left_arrow = 0x1E # A key
-hex_right_arrow = 0x20 # D key
 
 eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 head_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -96,10 +49,10 @@ while 1:
         print(f"{total_frames}. CENTER")
         if(go_left is True):
             #keyboard.release(Key.left)
-            ReleaseKey(hex_left_arrow)
+            wk.ReleaseKey(wk.hex_left_arrow)
         if(go_right is True):
             #keyboard.release(Key.right)
-            ReleaseKey(hex_right_arrow)
+            wk.ReleaseKey(wk.hex_right_arrow)
         go_left = False
         go_right = False
     
@@ -109,8 +62,8 @@ while 1:
         if(go_left is False):
             #keyboard.press(Key.left)
             #keyboard.release(Key.right)
-            PressKey(hex_left_arrow)
-            ReleaseKey(hex_right_arrow)
+            wk.PressKey(wk.hex_left_arrow)
+            wk.ReleaseKey(wk.hex_right_arrow)
             go_left = True  
     elif(len(eyes_rotate_right) >= 2):
         print(f"{total_frames}. RIGHT")
@@ -118,8 +71,8 @@ while 1:
         if(go_right is False):
             #keyboard.press(Key.right)
             #keyboard.release(Key.left)
-            PressKey(hex_right_arrow)
-            ReleaseKey(hex_left_arrow)
+            wk.PressKey(wk.hex_right_arrow)
+            wk.ReleaseKey(wk.hex_left_arrow)
             go_right = True
     
 
@@ -133,7 +86,6 @@ while 1:
     key = cv2.waitKey(1)
     if key == 27:
         break
-
 
 cap.release()
 cv2.destroyAllWindows()
