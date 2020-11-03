a51484078d8e75714e7175d5af3b6e3e86520b6a
nciganovic <nciganovic99@gmail.com>
2020-11-03 18:31:01 +0100

    change variable names

--- driver.py
+++ driver.py
@@ -7,7 +7,7 @@ from pynput.keyboard import Key, Controller
 if(platform.system() == "Windows"):
     import winkeys as wk #Keaboard keys for playing games on Windows
 
-eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+#eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 head_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 cap = cv2.VideoCapture(0)
@@ -40,20 +40,20 @@ while 1:
     gray_rotate_left = cv2.cvtColor(rotate_frame_left, cv2.COLOR_BGR2GRAY)
     gray_rotate_right = cv2.cvtColor(rotate_frame_right, cv2.COLOR_BGR2GRAY)
     
-    eyes = head_cascade.detectMultiScale(gray, 1.3, 5)
-    eyes_rotate_left = head_cascade.detectMultiScale(gray_rotate_left, 1.3, 5)
-    eyes_rotate_right = head_cascade.detectMultiScale(gray_rotate_right, 1.3, 5)
+    head = head_cascade.detectMultiScale(gray, 1.3, 5)
+    head_rotate_left = head_cascade.detectMultiScale(gray_rotate_left, 1.3, 5)
+    head_rotate_right = head_cascade.detectMultiScale(gray_rotate_right, 1.3, 5)
 
-    for (ex,ey,ew,eh) in eyes:
+    for (ex,ey,ew,eh) in head:
         cv2.rectangle(frame, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
     
-    for (ex,ey,ew,eh) in eyes_rotate_left:
+    for (ex,ey,ew,eh) in head_rotate_left:
         cv2.rectangle(rotate_frame_left, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
     
-    for (ex,ey,ew,eh) in eyes_rotate_right:
+    for (ex,ey,ew,eh) in head_rotate_right:
         cv2.rectangle(rotate_frame_right, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
     
-    if(len(eyes) >= 1):
+    if(len(head) >= 1):
         print(f"{total_frames}. CENTER")
         if(go_left is True):
             keyboard.release(Key.left)
@@ -64,7 +64,7 @@ while 1:
         go_left = False
         go_right = False
     
-    elif(len(eyes_rotate_left) >= 1):
+    elif(len(head_rotate_left) >= 1):
         print(f"{total_frames}. LEFT")
         go_right = False
         if(go_left is False):
@@ -73,7 +73,7 @@ while 1:
             #wk.PressKey(wk.hex_left_arrow)
             #wk.ReleaseKey(wk.hex_right_arrow)
             go_left = True  
-    elif(len(eyes_rotate_right) >= 1):
+    elif(len(head_rotate_right) >= 1):
         print(f"{total_frames}. RIGHT")
         go_left = False
         if(go_right is False):
